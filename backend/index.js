694168efd95e45c1f881012bfb84320ac597813a
vsol <vsol.sil@gmail.com>
2023-04-20 11:11:20 -0300

    Initialize backend

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+const app = express();
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
