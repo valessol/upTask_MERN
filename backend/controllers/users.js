0c213a0cc59890fd92d7b7b48a30adb395b7a823
vsol <vsol.sil@gmail.com>
2023-04-20 12:44:41 -0300

    prevent to save duplicated emails

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,6 +3,13 @@ import User from "../models/User.js";
 export const getUsers = async (req, res) => {};
 
 export const register = async (req, res) => {
+  const { email } = req.body;
+  const existUser = await User.findOne({ email });
+
+  if (existUser) {
+    const error = new Error("Usuario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
   try {
     const user = new User(req.body);
     const savedUser = await user.save();
