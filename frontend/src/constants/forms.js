9e94f75b7914e5337f0069f683cbf43fbbcc9d5b
vsol <vsol.sil@gmail.com>
2023-05-02 14:48:17 -0300

    create Fields component

--- frontend/src/constants/forms.js
+++ frontend/src/constants/forms.js
@@ -1,25 +1,57 @@
 export const newProjectForm = [
   {
     id: "name",
+    element: "input",
     type: "text",
     placeholder: "Diseño de logotipo",
     label: "Nombre del proyecto",
   },
   {
     id: "description",
-    type: "textarea",
+    element: "textarea",
     placeholder: "Diseño de logotipo",
     label: "Descripción",
   },
   {
     id: "finishDate",
+    element: "input",
     type: "date",
     label: "Fecha de entrega",
   },
   {
     id: "client",
+    element: "input",
     type: "text",
     placeholder: "John Doe",
     label: "Nombre del cliente",
+  },
+];
+
+export const newTaskForm = [
+  {
+    id: "name",
+    element: "input",
+    type: "text",
+    placeholder: "Definición de paleta de colores",
+    label: "Nombre de la tarea",
+  },
+  {
+    id: "description",
+    element: "textarea",
+    type: "textarea",
+    placeholder: "Seleccionar la paleta adecuada para la marca",
+    label: "Descripción",
+  },
+  {
+    id: "finishDate",
+    element: "input",
+    type: "date",
+    label: "Fecha de entrega",
+  },
+  {
+    id: "priority",
+    element: "select",
+    label: "Prioridad",
+    options: ["Baja", "Media", "Alta"],
   },
 ];
